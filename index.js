c35d187c34f0f6c95c635a23e7673323dd2f2180
YoungWoo Kim <38308305+kher1128@users.noreply.github.com>
2022-01-23 20:10:39 +0900

    finish api

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+
 const authRouter = require('./routes/auth')
 const userRouter = require('./routes/user')
+const movieRouter = require('./routes/movie')
+const listRouter = require('./routes/lists')
 
 dotenv.config();
 
@@ -20,6 +23,8 @@ mongoose
 app.use(express.json())
 app.use('/api/auth', authRouter)
 app.use('/api/user', userRouter)
+app.use('/api/movie', movieRouter)
+app.use('/api/list', listRouter)
 
 app.listen(process.env.NODE_PORT, () => {
   console.log(`${process.env.NODE_PORT} : is listening`)
